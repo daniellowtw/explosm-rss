1a11b74a055beffc58139ecf02d4e3e67526a4c2
Daniel Low <daniellowtw@gmail.com>
2017-08-24 23:27:46 +0100

    Fix searching for image when new lines are replaced by spaces

--- explosm/explosm_test.go
+++ explosm/explosm_test.go
@@ -2,12 +2,11 @@ package explosm
 
 import (
 	"testing"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
-func TestRegexp(t *testing.T) {
-	var validResponse = `<li><a href="/comics/latest/" class="" title="Latest comic"><img src="/img/nav-button_newest@2x.png"/></a></li>
+var testCases = map[string]string{
+	"with sapces": `<li><a class="disabled" title="Latest comic"><img src="/img/nav-button_newest@2x.png"/></a></li> </ul> </div> </div> </div> <div id="comic-container"> <div class="row"> <div class="small-12 medium-12 large-12 columns"> <img id="main-comic" src="//files.explosm.net/comics/Kris/mailman2.png?t=0AB34F" /> </div> </div> </div> <div class="row"> <div class="small-12 medium-6 large-6 columns"> <div class="row collapse">`,
+	"with new lines": `<li><a href="/comics/latest/" class="" title="Latest comic"><img src="/img/nav-button_newest@2x.png"/></a></li>
 </ul>
 </div>
 </div>
@@ -23,7 +22,16 @@ func TestRegexp(t *testing.T) {
 </div>
 <div class="row">
 <div class="small-12 medium-6 large-6 columns">
-<div class="row collapse">`
-	res := r.FindSubmatch([]byte(validResponse))
-	spew.Dump(res)
+<div class="row collapse">`,
 }
+
+func TestRegexp(t *testing.T) {
+	for name, tc := range testCases {
+		res := FindComicURL([]byte(tc))
+		if res == "" {
+			t.Logf("failed to find image for test case %s", name)
+			t.Fail()
+		}
+	}
+}
+
