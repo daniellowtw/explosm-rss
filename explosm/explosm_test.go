a9769fe071c1586d0970f11508112de141b629c8
Daniel Low <daniellowtw@gmail.com>
2018-05-01 19:34:13 +0100

    Update for 2018

--- explosm/explosm_test.go
+++ explosm/explosm_test.go
@@ -2,36 +2,18 @@ package explosm
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-var testCases = map[string]string{
-	"with sapces": `<li><a class="disabled" title="Latest comic"><img src="/img/nav-button_newest@2x.png"/></a></li> </ul> </div> </div> </div> <div id="comic-container"> <div class="row"> <div class="small-12 medium-12 large-12 columns"> <img id="main-comic" src="//files.explosm.net/comics/Kris/mailman2.png?t=0AB34F" /> </div> </div> </div> <div class="row"> <div class="small-12 medium-6 large-6 columns"> <div class="row collapse">`,
-	"with new lines": `<li><a href="/comics/latest/" class="" title="Latest comic"><img src="/img/nav-button_newest@2x.png"/></a></li>
-</ul>
+func Test2018(t *testing.T) {
+	var response = `<div id="main-left">
+<section id="comic-area">
+<div id="comic-wrap">
+<img id="main-comic" src="//files.explosm.net/comics/Dave/genieweek3.png?t=06538A">
 </div>
-</div>
-</div>
-<div id="comic-container">
-<div class="row">
-<div class="small-12 medium-12 large-12 columns">
-<a href="http://explosm.net/show/episode/201/eden">
-<img id="main-comic" src="//files.explosm.net/comics/Dave/Eden_Thumbnail_728x410.png?t=FDF100"/>
-</a>
-</div>
-</div>
-</div>
-<div class="row">
-<div class="small-12 medium-6 large-6 columns">
-<div class="row collapse">`,
-}
-
-func TestRegexp(t *testing.T) {
-	for name, tc := range testCases {
-		res := FindComicURL([]byte(tc))
-		if res == "" {
-			t.Logf("failed to find image for test case %s", name)
-			t.Fail()
-		}
-	}
+<div id="comic-under">
+`
+	assert.True(t, imgRegexp.MatchString(response), "Regex has changed")
 }
 
